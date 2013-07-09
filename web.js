e1fc9de64fba42085db97b4e43a909b8436b8b7a
Tan Tran <tankimtran@gmail.com>
2013-07-09 16:48:18 -0700

    web.js now reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs')
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var filename = 'index.html';
+	stringToWrite = fs.readFileSync(filename, 'utf-8');
+	response.send(stringToWrite);
 });
 
 var port = process.env.PORT || 5000;
